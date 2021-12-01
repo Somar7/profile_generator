d0cea07581a6a6a65067516881bd92144bb205e9
somar7 <somar7@nait.ca>
2021-12-01 01:16:54 +0000

    Survey exercise

--- survey.js
+++ survey.js
@@ -1,26 +1,38 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 let result = [];
-rl.question("What's your name? Nicknames are also acceptable :)", (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-  result.push(answer);
-    rl.question("What's an activity you like doing? :)", (answer) => {
-        console.log(`Thank you for your valuable feedback: ${answer}`);
-        result.push(answer);
-        rl.question("What do you listen to while doing that? :)", (answer) => {
-            console.log(`Thank you for your valuable feedback: ${answer}`);
-            result.push(answer);
-            rl.question("Which meal is your favourite (eg: dinner, brunch, etc.) :)", (answer) => {
-                console.log(`Thank you for your valuable feedback: ${answer}`);
-                result.push(answer);
-                console.log(result);
-                
-                rl.close();
-            });
-        });
-    });
-});
+rl.question('Whats your name? Nicknames are also acceptable :)', (answer) => {
+  console.log(`Thank you for your valuable feeback${answer}.`);
+  result.push(answer);
+  rl.question('What is an activity you like doing?', (answer) => {
+    console.log(`You like doing ${answer}.`);
+    result.push(answer);
+    rl.question('What do you listen to while doing that?', (answer) => {
+      console.log(`You like listening to ${answer}.`);
+      result.push(answer);
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)', (answer) => {
+        console.log(`Your favourite meal is ${answer}.`);
+        result.push(answer);
+        rl.question('What is your favourite thing to eat for that meal?', (answer) => {
+          console.log(`Your favourite thing to eat for that meal is: ${answer}.`);
+          result.push(answer);
+          rl.question('Which sport is your absolute favourite?', (answer) => {
+            console.log(`Your favourite sport is ${answer}.`);
+            result.push(answer);
+            rl.question('What is your superpower? In a few words, tell us what you are amazing at!', (answer) => {
+              console.log(`Your superpower is:${answer}.`);
+              result.push(answer);
+              console.log(result);
+
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+}); 
